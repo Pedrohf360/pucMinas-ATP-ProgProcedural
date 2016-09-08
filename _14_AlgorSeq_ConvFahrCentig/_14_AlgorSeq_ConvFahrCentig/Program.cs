e9573eccb548f4e6c49a0b7ba5e91b698de00ebf
Pedro Fonseca <pedro.henriqueff10@gmail.com>
2016-09-07 22:02:05 -0300

    Conversor de °F para °C.

---
+++ _14_AlgorSeq_ConvFahrCentig/_14_AlgorSeq_ConvFahrCentig/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_AlgorSeq_ConvFahrCentig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIÁVEIS
+            float grFahr, grCent;
+
+            Console.Write("Digite o valor em °F (graus Fahrenheit) que deseja converter para °C (graus Celsius).: ");
+            grFahr = float.Parse(Console.ReadLine());
+
+            grCent = (grFahr - 32) * 5 / 9;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n{0}°F convertido para °C = {1}°C.", grFahr, grCent);
+            Console.ResetColor();
+
+            Console.WriteLine("\nPressione qualquer tecla para sair.");
+            Console.ReadKey(true);  
+        }
+    }
+}
