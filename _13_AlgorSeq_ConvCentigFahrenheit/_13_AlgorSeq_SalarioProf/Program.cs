3b3228d80d6220bee0b73d2399d048f7ad0079fd
Pedro Fonseca <pedro.henriqueff10@gmail.com>
2016-09-07 21:52:22 -0300

    Conversor de °C para °F.

---
+++ _13_AlgorSeq_ConvCentigFahrenheit/_13_AlgorSeq_SalarioProf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_AlgorSeq_ConvCentigFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //VARIÁVEIS
+            float grausCent, grausFahr;
+
+            //ENTRADA
+            Console.Write("Quantos graus Centígrados está fazendo? ");
+            grausCent = float.Parse(Console.ReadLine());
+
+            grausFahr = (9 * grausCent + 160) / 5;
+
+            //SAÍDA
+            Console.WriteLine("\nA conversão de graus Centígrados para Fahrenheit fica assim: {0}°C = {1}°F.", grausCent, grausFahr);
+
+            Console.WriteLine("\nPressione qualquer tecla para sair.");
+            Console.ReadKey(true);
+
+        }
+    }
+}
