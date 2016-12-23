566302f666b3d8e78b99e6f7f5d4d6f7bdb734e3
Pedro H <pedro.henriqueff10@gmail.com>
2016-12-23 00:02:38 -0200

    Exercícios - Lista 3, continuação.

---
+++ 2016_12_11_Produtos/2016_12_11_Produtos/Program.cs
@@ -0,0 +1,128 @@
+﻿/* 1. Faça um programa que receba o nome de cinco produtos e seus respectivos preços, calcule e mostre:
+ a quantidade de produtos com preço inferior a R$ 50,00;
+ o nome dos produtos com preço entre R$ 50,00 e R$ 100.00;
+ a média dos preços dos produtos com preço superior a R$ 100.00.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_12_11_Produtos
+{
+    class Program
+    {
+        struct Produto
+        {
+            public string nome;
+            public double preco;
+        }
+
+        static void PreencherCadProd(Produto[] nomeStruct)
+        {
+            int cont = 0;
+            do
+            {
+                Console.Write("Nome: ");
+                nomeStruct[cont].nome = Console.ReadLine();
+
+                Console.Write("\nPreço: ");
+                nomeStruct[cont].preco = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(new string('-', 30));
+
+                cont++;
+            } while (cont < nomeStruct.Length);
+        }
+
+        // Retorna quantidade de valores maiores ou menores do que o informado
+        static int QtdMaiorMenor(Produto[] nomeStruct, int informeValor, string maiorMenor)
+        {
+            int qtdMaior = 0, qtdMenor = 0;
+
+            maiorMenor.ToUpper();
+
+            if (maiorMenor == "menor")
+            {
+                for (int i = 0; i < nomeStruct.Length; i++)
+                {
+                    if (nomeStruct[i].preco < informeValor)
+                    {
+                        qtdMenor++;
+                    }
+                }
+                return qtdMenor;
+            }
+            else
+            {
+                for (int i = 0; i < nomeStruct.Length; i++)
+                {
+                    if (nomeStruct[i].preco > informeValor)
+                    {
+                        qtdMaior++;
+                    }
+                }
+                return qtdMaior;
+            }
+
+            return 0;
+        }
+
+        static double SomaValores(Produto[] nomeStruct)
+        {
+            double soma = 0;
+
+            for (int i = 0; i < nomeStruct.Length; i++)
+            {
+                if (nomeStruct[i].preco > 100) soma += nomeStruct[i].preco;
+            }
+
+            return soma;
+        }
+
+        static void NomeProdEntreXeY(Produto[] nomeStruct, double x, double y)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < nomeStruct.Length; i++)
+            {
+                if (nomeStruct[i].preco >= x && nomeStruct[i].preco <= y)
+                {
+                    Console.WriteLine(i + 1 + ") " + nomeStruct[i].nome);
+                    Console.WriteLine();
+                    cont++;
+                }
+            }
+
+            if (cont == 0) Console.WriteLine("\nNão há nenhum produto com preço entre {0} e {1}.", x, y);
+        }
+
+        static void Main(string[] args)
+        {
+            int qtdInf50, qtdSup100;
+            double somaPrecos, mediaSup100 = 0;
+
+            Produto[] cadProd = new Produto[5];
+
+            PreencherCadProd(cadProd);
+
+            qtdInf50 = QtdMaiorMenor(cadProd, 50, "menor");
+            Console.WriteLine("\nQuantidade de produtos com preço inferior a R$50, 00: {0}.", qtdInf50);
+
+            qtdSup100 = QtdMaiorMenor(cadProd, 100, "maior");
+            somaPrecos = SomaValores(cadProd);
+            
+            if (qtdSup100 != 0) mediaSup100 = somaPrecos / qtdSup100;
+
+            Console.WriteLine("\nMédia da soma dos produtos com preço superior a R$100, 00: {0}.", mediaSup100);
+
+            Console.WriteLine("\nNome dos produtos com preço entre R$50, 00 e R$100, 00:");
+            NomeProdEntreXeY(cadProd, 50, 100);
+
+            Console.WriteLine("\n\nPressione qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2016_12_22_CaracFisicasPesquisa/2016_12_22_CaracFisicasPesquisa/Program.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * 3. Foi realizada uma pesquisa de algumas características físicas de 50 habitantes de uma certa região. De
+cada habitante foram coletados os seguintes dados: sexo, altura, idade e cor dos olhos (A - Azuis, V -
+Verdes ou C - Castanhos).
+Faça um programa que leia esses dados e armazene-os em um registro do tipo vetor. Determine:
+(a) a média de idade das pessoas com olhos castanhos e altura superior a 1.60 m;
+(b) a maior idade entre os habitantes;
+(c) a quantidade de indivíduos do sexo feminino cuja idade esteja entre 20 e 45 anos (inclusive) ou
+que tenham olhos verdes e altura inferior a 1,70 m;
+(d) o percentual de homens.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016_12_22_CaracFisicasPesquisa
+{
+    class Program
+    {
+        struct CaracFisicas
+        {
+            public char sexo;
+            public float altura;
+            public int idade;
+            public char corOlhos;
+        }
+        
+        static void PreencheVetor(char[] sexo, float[] altura, int[] idade, char[] corOlhos, CaracFisicas[] nomePesq)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < nomePesq.Length; i++)
+            {
+                nomePesq[i].sexo = sexo[r.Next(0, 2)]; // M ou F
+                nomePesq[i].altura = altura[r.Next(0, 4)]; // 4 valores para altura
+                nomePesq[i].idade = idade[r.Next(0, 6)]; // 6 idades
+                nomePesq[i].corOlhos = corOlhos[r.Next(0, 3)]; // A, V ou C
+            }
+        }
+
+        static void LerStruct(CaracFisicas[] nomePesq)
+        {
+            for (int i = 0; i < nomePesq.Length; i++)
+            {
+                Console.WriteLine(i+1 + ") Sexo:" + nomePesq[i].sexo);
+                Console.WriteLine("Cor dos olhos: " + nomePesq[i].corOlhos);
+                Console.WriteLine("Idade: " + nomePesq[i].idade);
+                Console.WriteLine("Altura: " + nomePesq[i].altura);
+                Console.WriteLine(new string('-', 40));
+            }
+        }
+
+        static int MenuOpcoes()
+        {
+            int opcao;
+
+            Console.WriteLine("Digite a opção desejada:\n\n1- Exibir informações do struct;\n2- Exibir a média de idade das pessoas com olhos castanhos e altura superior a 1.60 m\n3- Exibir a maior idade entre os habitantes;\n4- a quantidade de indivíduos do sexo feminino cuja idade esteja entre 20 e 45 anos (inclusive) ou que tenham olhos verdes e altura inferior a 1,70m;\n5- Exibir percentual de homens;\n6- Sair.");
+            opcao = int.Parse(Console.ReadLine());
+
+            while (opcao < 1 || opcao > 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O valor digitado não corresponde a nenhuma das opções. Tente novamente.");
+                Console.ResetColor();
+
+                Console.WriteLine("\n\nPressione qualquer tecla para prosseguir.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            return opcao;
+        }
+
+        static void Main(string[] args)
+        {
+            char[] sexo, corOlhos;
+            float[] altura;
+            int[] idade;
+            int opcao;
+            CaracFisicas[] pesq1;
+
+            Random r = new Random();
+                
+            pesq1 = new CaracFisicas[50];
+            sexo = new char[2] { 'M', 'F' };
+            corOlhos = new char[3] { 'A', 'V', 'C' };
+            altura = new float[4] { 1.50F, 1.30F, 1.70F, 1.90F };
+            idade = new int[6] { 10, 15, 20, 25, 30, 35 };
+
+            PreencheVetor(sexo, altura, idade, corOlhos, pesq1);
+
+            do
+            {
+                opcao = MenuOpcoes();
+                switch(opcao)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        LerStruct(pesq1);
+
+                        Console.WriteLine("\nPressione qualquer tecla para prosseguir.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                    break;
+
+                    case 2:
+                        Console.Clear();
+
+                        Console.WriteLine("\nPressione qualquer tecla para prosseguir.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+
+                        Console.WriteLine("\nPressione qualquer tecla para prosseguir.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        break;
+
+                    case 4:
+                        Console.Clear();
+
+                        Console.WriteLine("\nPressione qualquer tecla para prosseguir.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        break;
+
+                    case 5:
+                        Console.Clear();
+
+                        Console.WriteLine("\nPressione qualquer tecla para prosseguir.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        break;
+                }
+            } while (opcao != 6);
+        }
+    }
+}
