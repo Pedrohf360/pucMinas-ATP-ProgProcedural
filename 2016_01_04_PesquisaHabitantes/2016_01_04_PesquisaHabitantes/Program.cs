21cbc565500823944360d9dd903e90f023c94aff
Pedro H <pedro.henriqueff10@gmail.com>
2017-01-11 13:34:29 -0200

    2017_01_11_Quase pronto (falta implementar tratamento de erros).

--- 2016_01_04_PesquisaHabitantes/2016_01_04_PesquisaHabitantes/Program.cs
+++ 2016_01_04_PesquisaHabitantes/2016_01_04_PesquisaHabitantes/Program.cs
@@ -23,14 +23,21 @@ namespace _516_01_04_PesquisaHabitantes
                 Console.WriteLine(i + 1 + ") What is your age?");
                 nomeStruct[i].age = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("What is your sex? (1- Male, 2- Female)");
+                Console.WriteLine("\nWhat is your sex? (1- Male, 2- Female)");
                 nomeStruct[i].sex = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("How many money do you have?");
+                Console.WriteLine("\nHow many money do you have?");
                 nomeStruct[i].familiarMoney = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("How many childs do you have?");
+                Console.WriteLine("\nHow many childs do you have?");
                 nomeStruct[i].childNum = int.Parse(Console.ReadLine());
+
+                // Decrementa 1 inteiro da variável "i" se algum valor informativo estiver incorreto, desta forma, a posição em que os dados incorretos
+                // foram digitados será corrigida.
+                if ()
+                {
+                    i--;
+                }
 
                 Console.Clear();
             }
@@ -42,10 +49,10 @@ namespace _516_01_04_PesquisaHabitantes
 
             try
             {
-                Console.WriteLine("Digite a opção desejada:\n\n1- Média de salário entre os habitantes;\n2- Maior e menor idade entre o grupo;\n3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00.");
+                Console.WriteLine("Digite a opção desejada:\n\n1- Média de salário entre os habitantes;\n2- Maior e menor idade entre o grupo;\n3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00;\n4- Sair.");
                 opcao = int.Parse(Console.ReadLine());
 
-                while (opcao < 1 || opcao > 3)
+                while (opcao < 1 || opcao > 4)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Opção inválida. Pressione qualquer tecla e tente novamente.");
@@ -53,7 +60,7 @@ namespace _516_01_04_PesquisaHabitantes
                     Console.Clear();
                     Console.ResetColor();
 
-                    Console.WriteLine("Digite a opção desejada:\n\n1- Média de salário entre os habitantes;\n2- Maior e menor idade entre o grupo;\n3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00.");
+                    Console.WriteLine("Digite a opção desejada:\n\n1- Média de salário entre os habitantes;\n2- Maior e menor idade entre o grupo;\n3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00;\n4- Sair.");
                     opcao = int.Parse(Console.ReadLine());
                     Console.Clear();
                 }
@@ -94,12 +101,31 @@ namespace _516_01_04_PesquisaHabitantes
             return media;
         }
 
+        // O programador pode escolher se deseja obter a maior ou a menor idadae do struct CaracHab[]
+        static int MaiorMenorIdade(CaracHab[] nomeStruct, string maiorOuMenor)
+        {
+            int maior = 0, menor = 150;
+            maiorOuMenor.ToUpper();
+
+            for (int i = 0; i < nomeStruct.Length; i++)
+            {
+                if (nomeStruct[i].age > maior) maior = nomeStruct[i].age;
+                if (nomeStruct[i].age < menor) menor = nomeStruct[i].age;
+            }
+
+            if (maiorOuMenor == "maior") return maior;
+            if (maiorOuMenor == "menor") return menor;
+
+            return -1;
+        }
+
         static void Main(string[] args)
         {
-            int habitNum = 5, opcao;
+            Console.SetWindowSize(100, 25);
+            int habitNum = 3, opcao, quantidadeMulherMais2filhos = 0;
             double media;
 
-            CaracHab[] search1 = new CaracHab[5];
+            CaracHab[] search1 = new CaracHab[habitNum];
 
             PreencherStruct(search1);
 
@@ -117,21 +143,36 @@ namespace _516_01_04_PesquisaHabitantes
                         Console.WriteLine("A média de salário entre os habitantes é de: R${0:N2}", media);
 
                         Console.WriteLine("\n\nPressione qualquer tecla para continuar.");
-                        Console.ReadLine();
+                        Console.ReadKey();
                         break;
 
                     //2- Maior e menor idade entre o grupo
                     case 2:
+                        Console.WriteLine("Maior idade entre o grupo: {0} anos.", MaiorMenorIdade(search1, "maior"));
+
+                        Console.WriteLine("Menor idade entre o grupo: {0} anos.", MaiorMenorIdade(search1, "menor"));
+
+                        Console.WriteLine("\n\nPressione qualquer tecla para continuar.");
+                        Console.ReadKey();
                         break;
 
-                    //3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00.ssssss
+                    //3- Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00
                     case 3:
+                        for (int i = 0; i < search1.Length; i++)
+                        {
+                            if (search1[i].childNum > 2 && search1[i].familiarMoney < 600) quantidadeMulherMais2filhos++;
+                        }
+
+                        Console.WriteLine("Quantidade de mulheres com mais de 2 filhos e renda familiar inferior a R$600, 00: {0} mulheres.", quantidadeMulherMais2filhos);
+
+                        Console.WriteLine("\n\nPressione qualquer tecla para continuar.");
+                        Console.ReadKey();
                         break;
 
                 }
-            } while (opcao !=  5);
+            } while (opcao !=  4);
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
