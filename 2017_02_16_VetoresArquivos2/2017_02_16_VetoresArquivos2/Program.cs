37309dba4f96442028515e9fae264444d5f88ff7
Pedro Fonseca <pedro.henriqueff10@gmail.com>
2017-02-16 17:22:00 -0200

    Aula 02 LPOO - Vetores e Arquivos (trabalhando c/ classes).

---
+++ 2017_02_16_VetoresArquivos2/2017_02_16_VetoresArquivos2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*2) Uma empresa de vendas oferece a seus clientes descontos em função do valor da
+compra efetuada pelo mesmo. Esse desconto é de 20%, se o valor da compra for
+maior ou igual a R$1.800,00; e de 15% para compras de valores menores. Faça
+um programa em C# que leia um arquivo texto, de nome “vendasClientes.txt”, que
+contém, em cada linha, os seguintes dados, separados pelo caractere “-”: nome do
+cliente; valor da compra efetuada pelo cliente. Esse programa deve calcular e
+imprimir, em um outro arquivo texto, de nome “descontosClientes.txt”, o nome do
+cliente e o desconto obtido por ele.
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_02_16_VetoresArquivos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
