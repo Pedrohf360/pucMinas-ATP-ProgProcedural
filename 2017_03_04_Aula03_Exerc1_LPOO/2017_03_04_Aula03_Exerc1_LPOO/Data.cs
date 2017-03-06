af661d655ea8af27f69fbf087a2c944c5901f2b7
Pedro Fonseca <pedro.henriqueff10@gmail.com>
2017-03-06 14:44:32 -0300

    Exerc. LPOO - Aula 03.

---
+++ 2017_03_04_Aula03_Exerc1_LPOO/2017_03_04_Aula03_Exerc1_LPOO/Data.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_03_04_Aula03_Exerc1_LPOO
+{
+    class Data
+    {
+        private int dia, mes, ano;
+
+        public static int[] diasMeses;
+
+        private void Init(int d, int m, int a)
+        {
+            this.dia = d;
+            this.mes = m;
+            this.ano = a;
+        }
+
+        public Data()
+        {
+            Init(1, 1, 2001);
+        }
+
+        public Data(int d, int m, int a)
+        {
+            Init(d, m, a);
+        }
+
+        public bool VerificarData(int[] diasMeses, int d, int m, int a)
+        {
+            if ((d < 1 || d > diasMeses[m - 1]) || (m < 1 || m > 12) || (a < 1)) return false;
+
+            return true;
+        }
+
+        public static int[] VerificaAnoBissexto(int ano)
+        {
+            int[] anoComum, anoBissexto;
+
+            anoComum = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            anoBissexto = anoComum;
+
+            anoBissexto[1] = 29;
+
+            if (DateTime.IsLeapYear(ano) == true) return anoBissexto;
+
+            return anoComum;
+        }
+    }
+}
---
+++ 2017_03_04_Aula03_Exerc1_LPOO/2017_03_04_Aula03_Exerc1_LPOO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_03_04_Aula03_Exerc1_LPOO
+{
+    class Program
+    {
+        static void SplitDataTexto(string dataTexto, out int d, out int m, out int a)
+        {
+            String[] dataTextoVetor;
+
+            dataTextoVetor = dataTexto.Split('/');
+
+            d = int.Parse(dataTextoVetor[0]);
+            m = int.Parse(dataTextoVetor[1]);
+            a = int.Parse(dataTextoVetor[2]); 
+        }
+
+        static void PedirDataUsuario()
+        {
+            string dataTexto;
+
+            int d, m, a;
+
+            do
+            {
+                Console.WriteLine("Digite uma data qualquer (Ex.: 01/01/2000):");
+                dataTexto = Console.ReadLine();
+
+                SplitDataTexto(dataTexto, out d, out m, out a);
+            } while (ver);
+        }
+
+        static void Main(string[] args)
+        {
+            String dataTexto;
+            int dia, mes, ano;
+
+            SplitDataTexto(dataTexto, out dia, out mes, out ano);
+
+            Data.diasMeses = Data.VerificaAnoBissexto(ano);
+
+            Data data1 = new Data(dia, mes, ano);
+
+
+
+            Console.WriteLine("\nPressione qualquer tecla para sair.");
+            Console.ReadKey(true);  
+        }
+    }
+}
