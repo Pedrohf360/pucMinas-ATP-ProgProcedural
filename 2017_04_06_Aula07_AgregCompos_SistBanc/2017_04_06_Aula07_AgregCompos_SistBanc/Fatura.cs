7a076a863a876caea42c6d0f936f224d9a451a2f
Pedrohf360 <pedrohf360@gmail.com>
2017-04-06 16:11:41 -0300

    2017_04_06_Aula07 - Exerc. 2.

---
+++ 2017_04_06_Aula07_AgregCompos_SistBanc/2017_04_06_Aula07_AgregCompos_SistBanc/Fatura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_04_06_Aula07_AgregCompos_SistBanc
+{
+    class Fatura
+    {
+        double total;
+
+        public Fatura(double total)
+        {
+            this.total = total;
+        }
+
+        public void adiciona(double valor) 
+        {
+            total += valor;
+        }
+
+
+        public double calculaMulta() 
+        {
+            return total * 0.02;
+        }
+        
+        public void imprimeDados() 
+        {
+            Console.WriteLine("Total fatura: " + this.total);
+        }
+
+    }
+}
